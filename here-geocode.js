2d995cc2a4630a9d9d08da1796cc94379a43d0f3
JohnWalicki <johnwalicki@gmail.com>
2020-09-13 21:46:47 -0400

    Remove lon-lat variable and config node

--- here-geocode.js
+++ here-geocode.js
@@ -1,20 +1,8 @@
 module.exports = function(RED) {
-  function HEREapikeyNode(n) {
-    RED.nodes.createNode(this,n);
-    var node = this;
-    //node.apikey = n.apikey;
-  }
-  RED.nodes.registerType("hereapikey", HEREapikeyNode, {
-    credentials: {
-      apikey: {type: "password"}
-    }
-  });
-
   function HEREGeoSearch( n ) {
     RED.nodes.createNode(this,n );
     var node = this;
     var name = n.name;
-    var lon_lat = n.lon_lat;
     var query = n.query;
     var HEREConfigNode;
     var apiKey;
@@ -27,13 +15,6 @@ module.exports = function(RED) {
     node.on('input', function (msg) {
 
       msg.hereparams = msg.hereparams || {};
-
-      if( typeof msg.hereparams.lon_lat == 'undefined' ) {
-        msg.hereparams.lon_lat = lon_lat; // take the default or the node setting
-      } else {
-        // passed in param, override default or node setting
-        msg.hereparams.lon_lat = msg.hereparams.lon_lat;
-      }
 
       if( typeof msg.hereparams.query == 'undefined' ) {
         msg.hereparams.query = query; // take the default or the node setting
